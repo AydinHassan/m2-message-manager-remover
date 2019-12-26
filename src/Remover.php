a3929a5ea1756afa2c1df1bf1abc1dbc5828be5e
Aydin Hassan <aydin@hotmail.co.uk>
2019-12-26 21:16:29 +0100

    Code

---
+++ src/Remover.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TrashPanda\MessageManagerRemover;
+
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Message\MessageInterface;
+
+class Remover
+{
+    /**
+     * @var ManagerInterface
+     */
+    private $messageManager;
+
+    public function __construct(ManagerInterface $messageManager)
+    {
+        $this->messageManager = $messageManager;
+    }
+
+    public function removeByRegex(string $pattern): int
+    {
+        $matches = 0;
+        foreach ($this->messageManager->getMessages()->getItems() as $message) {
+            if (preg_match($pattern, $message->getText())) {
+                $this->removeMessage($message);
+                $matches++;
+            }
+        }
+
+        return $matches;
+    }
+
+    public function removeLastAddedMessage(): void
+    {
+        $message = $this->messageManager
+            ->getMessages()
+            ->getLastAddedMessage();
+
+        if (!$message) {
+            throw new \RuntimeException('No message found');
+        }
+
+        $this->removeMessage($message);
+    }
+
+    private function removeMessage(MessageInterface $message): void
+    {
+        $id = bin2hex(random_bytes(10));
+        $message->setIdentifier($id);
+
+        $this->messageManager
+            ->getMessages()
+            ->deleteMessageByIdentifier($id);
+    }
+}
---
+++ test/RemoverTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TrashPanda\MessageManagerRemoverTest;
+
+use Magento\Framework\Message\Collection;
+use Magento\Framework\Message\Manager;
+use Magento\Framework\Message\Notice;
+use PHPUnit\Framework\TestCase;
+use TrashPanda\MessageManagerRemover\Remover;
+
+class RemoverTest extends TestCase
+{
+    /**
+     * @var Collection
+     */
+    private $messageCollection;
+
+    /**
+     * @var Remover
+     */
+    private $remover;
+
+    public function setUp(): void
+    {
+        $this->messageCollection = new Collection();
+        $manager = $this->createMock(Manager::class);
+        $manager->expects($this->any())
+            ->method('getMessages')
+            ->willReturn($this->messageCollection);
+
+        $this->remover = new Remover($manager);
+    }
+    
+    public function testRemoveLastAddedMessageThrowsExceptionIfNoMessageFound(): void
+    {
+        self::expectException(\RuntimeException::class);
+        self::expectExceptionMessage('No message found');
+
+        $this->remover->removeLastAddedMessage();
+    }
+
+    public function testCanRemoveLastAddedMessage(): void
+    {
+        $this->messageCollection->addMessage(new Notice('First Message'));
+        $this->messageCollection->addMessage(new Notice('Second Message'));
+
+        self::assertEquals(2, $this->messageCollection->getCount());
+
+        $this->remover->removeLastAddedMessage();
+
+        self::assertEquals(1, $this->messageCollection->getCount());
+    }
+
+    public function testRemoveByRegexMatchingOne(): void
+    {
+        $this->messageCollection->addMessage(new Notice('First Message'));
+        $this->messageCollection->addMessage(new Notice('Second Message'));
+
+        self::assertEquals(2, $this->messageCollection->getCount());
+
+        $numRemoved = $this->remover->removeByRegex('/First/');
+
+        self::assertEquals(1, $numRemoved);
+        self::assertEquals(1, $this->messageCollection->getCount());
+    }
+
+    public function testRemoveByRegexMatchingMultiple(): void
+    {
+        $this->messageCollection->addMessage(new Notice('First Message'));
+        $this->messageCollection->addMessage(new Notice('Second Message'));
+
+        self::assertEquals(2, $this->messageCollection->getCount());
+
+        $numRemoved = $this->remover->removeByRegex('/Message/');
+
+        self::assertEquals(2, $numRemoved);
+        self::assertEquals(0, $this->messageCollection->getCount());
+    }
+}
